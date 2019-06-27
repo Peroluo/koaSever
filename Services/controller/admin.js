f20032cda686906199d1c0cc83cba686faa8cac9
luoguoxiong <luoguoxiong@ipin.com>
2019-06-27 14:59:43 +0800

    change

--- Services/controller/admin.js
+++ Services/controller/admin.js
@@ -1,14 +1,32 @@
 import { Controller, RequestMapping } from '../decorator'
 import middleware from '../decorator/middleware'
 const { Required, Auth } = middleware
+import http from '../utils/Http'
 @Controller
 class Admin {
   @RequestMapping({ method: 'get', url: '/test/:id' })
-  @Required(['NAME'])
-  getUserInfo(ctx) {
-    console.log(ctx.params.id)
+  // url非必填,不填则是/getUserInfo
+  async getUserInfo(ctx) {
+    const data = await http.httpRequest(
+      'http://202.96.155.121:8888/api/auth/loginByMobile',
+      {
+        mobile: '15323807318',
+        password: '123456'
+      }
+    )
+    ctx.body = { ...data }
+  }
 
-    ctx.body = { a: 1, b: 2 }
+  @RequestMapping({ method: 'get' })
+  async getGoods(ctx) {
+    const data = await http.httpRequest(
+      'http://202.96.155.121:8888/api/goods/category',
+      {
+        id: '1008008'
+      },
+      'get'
+    )
+    ctx.body = { ...data }
   }
 }
 export default Admin
--- Services/utils/Http.js
+++ Services/utils/Http.js
@@ -1,16 +1,14 @@
 import axios from 'axios'
-// 从其他服务获取数据
 class Http {
   /**
    * 初始化Sever相关属性
    * @param timeout 请求过期时间
    */
-  constructor({ timeout = 10000, baseURL = '' }) {
+  constructor(timeout = 10000, baseURL = '') {
     this.Server = axios.create({
       baseURL,
       timeout,
-      withCredentials: true,
-      httpsAgent: new https.Agent({ keepAlive: true })
+      withCredentials: true
     })
     this.TIMEOUT = timeout || 10000
   }
@@ -24,17 +22,15 @@ class Http {
    * @returns {Promise<void>}
    */
   async httpRequest(url, params, method = 'post') {
-    return await request[method]({
-      url,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      json: true,
-      timeout: this.TIMEOUT,
-      body: params
-    })
+    const obj =
+      method === 'get'
+        ? { params }
+        : {
+            ...params
+          }
+    return (await this.Server[method](url, obj)).data
   }
 
   // 添加其他请求(文件上传....) todo
 }
-module.exports = Server
+module.exports = new Http()
